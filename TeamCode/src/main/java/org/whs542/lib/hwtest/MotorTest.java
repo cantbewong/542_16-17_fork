3c4fabd2a21202d9f065f63f77e40d1f1855f1c6
Amar G <amargandhi10@gmail.com>
2016-09-19 18:33:37 -0700

    Setup
    -Setup all of the folders
    -Created a simple motor test

---
+++ TeamCode/src/main/java/org/whs542/lib/hwtest/MotorTest.java
@@ -0,0 +1,39 @@
+package org.whs542.lib.hwtest;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Amar on 9/19/2016.
+ */
+public class MotorTest extends OpMode {
+
+    DcMotor motor1;
+    double power = 0.0;
+
+    @Override
+    public void init(){
+        motor1 = hardwareMap.dcMotor.get("motor1");
+    }
+
+    @Override
+    public void loop(){
+
+        if(power >= -1.0){
+            power += 0.01;
+        }
+        else if(power >= 1){
+            power += -0.01;
+        }
+
+        motor1.setPower(power);
+
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
