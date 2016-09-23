303583dcd80d3ec16765aca235d85d846f9f1456
Amar G <amargandhi10@gmail.com>
2016-09-23 16:05:45 -0700

    CubicTeleOpTest

---
+++ TeamCode/src/main/java/org/whs542/lib/hwtest/CubicTeleOpTest.java
@@ -0,0 +1,52 @@
+package org.whs542.lib.hwtest;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Moses Won
+ */
+
+@TeleOp(name = "CubicTeleOpTest", group = "Tests")
+
+
+public class CubicTeleOpTest extends OpMode{
+
+    DcMotor frontRight;
+    DcMotor frontLeft;
+    DcMotor backRight;
+    DcMotor backLeft;
+
+    @Override
+    public void init(){
+
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+
+    }
+
+    @Override
+    public void loop(){
+
+        /*
+         * Keep in mind that when the joystick is pushed all the way forward, gamepad1.right_stick_y returns -14
+         * The following two lines of code raise the joystick input to a power of 3, making speed control more precise
+         */
+
+        double rightScaledPower = -Math.pow(gamepad1.right_stick_y, 3);
+        double leftScaledPower = -Math.pow(gamepad1.left_stick_y, 3);
+
+        frontRight.setPower(rightScaledPower);
+        frontLeft.setPower(leftScaledPower);
+        backRight.setPower(rightScaledPower);
+        backLeft.setPower(leftScaledPower);
+
+        telemetry.addData("Right Power", rightScaledPower);
+        telemetry.addData("Left Power", leftScaledPower);
+
+
+    }
+}
