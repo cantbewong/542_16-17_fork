0f276a796ef136160889cc5bacb8616c98b45177
JiangdaZhao <jdz3927@gmail.com>
2016-10-01 15:40:28 -0700

    Created a Flywheel Class

---
+++ TeamCode/src/main/java/org/whs542/ftc2017/CubicTeleOp.java
@@ -0,0 +1,67 @@
+package org.whs542.ftc2017;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/**
+ * Created by Jiangda on 10/1/2016.
+ */
+
+@TeleOp(name = "CubicTeleOp", group = "HwTest")
+public class CubicTeleOp extends OpMode {
+
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+
+    private Flywheel flywheel;
+
+    @Override
+    public void init(){
+
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+
+        flywheel = new Flywheel(hardwareMap);
+
+
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+    }
+
+    @Override
+    public void loop(){
+
+    /*
+     * Keep in mind that when the joystick is pushed all the way forward, gamepad1.right_stick_y returns -14
+     * The following two lines of code raise the joystick input to a power of 3, making speed control more precise
+     */
+
+        double rightScaledPower = -Math.pow(gamepad1.right_stick_y, 3);
+        double leftScaledPower = -Math.pow(gamepad1.left_stick_y, 3);
+
+        frontRight.setPower(rightScaledPower);
+        frontLeft.setPower(leftScaledPower);
+        backRight.setPower(rightScaledPower);
+        backLeft.setPower(leftScaledPower);
+
+        flywheel.run(gamepad1.right_bumper);
+
+
+        telemetry.addData("Right Power", rightScaledPower);
+        telemetry.addData("Left Power", leftScaledPower);
+        telemetry.addData("Flywheel", flywheel.getStatus());
+
+
+    }
+}
+
+
---
+++ TeamCode/src/main/java/org/whs542/ftc2017/Flywheel.java
@@ -0,0 +1,45 @@
+package org.whs542.ftc2017;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by Jiangda on 10/1/2016.
+ */
+public class Flywheel {
+
+    private DcMotor rightFly;
+    private DcMotor leftFly;
+    private boolean status;
+
+    private Toggler flyToggler = new Toggler(2);
+
+   //HardwareMap: basically tells the program where each device is located on the robot. The name in argument(e.g. "rightFly") should correspond exactly to the name on the phone.
+    Flywheel(HardwareMap aMap){
+        aMap.dcMotor.get("rightFly");
+        aMap.dcMotor.get("leftFly");
+        status = false;
+    }
+
+    //The core of the flwheel program. Spins flywheels if right bumper is pressed, stops spinning if pressed again.
+    public void run(boolean b1){
+        flyToggler.changeState(b1);
+        if(flyToggler.currentState() == 1){
+            status = true;
+            rightFly.setPower(1);
+            leftFly.setPower(-1);
+                    }
+        else{
+            status = false;
+            rightFly.setPower(0);
+            leftFly.setPower(0);
+        }
+    }
+    public String getStatus(){
+        if(status == true)
+            return "spinning";
+        else
+            return "not spinning";
+    }
+
+}
