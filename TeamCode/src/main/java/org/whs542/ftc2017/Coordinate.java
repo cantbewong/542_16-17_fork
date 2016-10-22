64f55e0760c127498a79415256c6d5f44dd0d1ae
JiangdaZhao <jdz3927@gmail.com>
2016-10-22 14:27:15 -0700

    Coordinate Class

---
+++ TeamCode/src/main/java/org/whs542/ftc2017/Coordinate.java
@@ -0,0 +1,38 @@
+package org.whs542.ftc2017;
+
+/**
+ * Created by Jiangda on 10/22/2016.
+ */
+public class Coordinate {
+    private double xPos;
+    private double yPos;
+    private double zPos;
+    private double orientation;
+
+    public Coordinate(double xPosition, double yPosition, double zPosition, double orientationInput){
+        xPos = xPosition;
+        yPos = yPosition;
+        zPos = zPosition;
+        orientation = orientationInput;
+    }
+
+    public Coordinate returnCoordValue(){
+        return this;
+    }
+
+    public double returnCoordSingleValue(String typeOfValue){
+        if(typeOfValue.equalsIgnoreCase("x")){
+            return xPos;
+        }
+        else if (typeOfValue.equalsIgnoreCase("y")) {
+
+            return yPos;
+        }
+        else if (typeOfValue.equalsIgnoreCase("z")){
+            return zPos;
+        }
+        else {
+            return orientation;
+        }
+    }
+}
