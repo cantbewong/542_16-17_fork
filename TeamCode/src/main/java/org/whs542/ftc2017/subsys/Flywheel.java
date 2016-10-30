32da6ed582bcdd84198e39243181ebd06fced082
Amar G <amargandhi10@gmail.com>
2016-10-29 17:25:40 -0700

    Flywheel Class Changes
    Added some methods for using the flywheel gate

--- TeamCode/src/main/java/org/whs542/ftc2017/subsys/Flywheel.java
+++ TeamCode/src/main/java/org/whs542/ftc2017/subsys/Flywheel.java
@@ -2,6 +2,7 @@ package org.whs542.ftc2017.subsys;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.whs542.lib.Coordinate;
 import org.whs542.lib.Toggler;
@@ -13,24 +14,24 @@ public class Flywheel {
 
     private DcMotor rightFly;
     private DcMotor leftFly;
+    private Servo flywheelGate;
     private boolean status;
+    private boolean gateStatus;
     private Coordinate currentPosition;
     private Coordinate Vortex=new Coordinate(304.8,304.8,304.8,1);
     double power = 1;                   //Defualt power level. Can be changed via the alternate contructor, or using the method setPower
 
     private Toggler flyToggler = new Toggler(2);
+    private Toggler gateToggler = new Toggler(2);
 
    //HardwareMap: basically tells the program where each device is located on the robot. The name in argument(e.g. "rightFly") should correspond exactly to the name on the phone.
     public Flywheel(HardwareMap aMap){
         rightFly = aMap.dcMotor.get("rightFly");
         leftFly = aMap.dcMotor.get("leftFly");
+        flywheelGate = aMap.servo.get("flywheelGate");
         status = false;
     }
-    public Flywheel(HardwareMap aMap, double powerIn){
-        rightFly = aMap.dcMotor.get("rightFly");
-        leftFly = aMap.dcMotor.get("leftFly");
-        status = false;
-    }
+
 
     //The core of the flywheel program. Spins flywheels if right bumper is pressed, stops spinning if pressed again.
     public void run(boolean b1){
@@ -46,11 +47,46 @@ public class Flywheel {
             leftFly.setPower(0);
         }
     }
+
+    public void run(boolean b1, double powerIn){
+        flyToggler.changeState(b1);
+        if(flyToggler.currentState() == 1){
+            status = true;
+            rightFly.setPower(-powerIn);
+            leftFly.setPower(powerIn);
+        }
+        else{
+            status = false;
+            rightFly.setPower(0);
+            leftFly.setPower(0);
+        }
+    }
+
+    public void operateGate(boolean a){
+        gateToggler.changeState(a);
+
+        if (gateToggler.currentState() == 1){
+            flywheelGate.setPosition(1.0);
+            gateStatus = true;
+        }
+        else{
+            flywheelGate.setPosition(0.0);
+            gateStatus = false;
+        }
+    }
+
     public String getStatus(){
         if(status)
             return "spinning";
         else
             return "not spinning";
+    }
+
+    public String getGateStatus(){
+        if(gateStatus)
+            return "open";
+        else
+            return "close";
     }
 
     public void setPower(double power){
--- TeamCode/src/main/java/org/whs542/ftc2017/subsys/WHSRobot.java
+++ TeamCode/src/main/java/org/whs542/ftc2017/subsys/WHSRobot.java
@@ -6,11 +6,15 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  * Created by Amar2 on 10/22/2016.
  */
 public class WHSRobot {
+
     public Drivetrain drivetrain;
     public Intake intake;
+    public Flywheel flywheel;
 
     public WHSRobot(HardwareMap robotMap){
         drivetrain = new Drivetrain(robotMap);
         intake = new Intake(robotMap);
+        flywheel = new Flywheel(robotMap);
     }
+
 }
